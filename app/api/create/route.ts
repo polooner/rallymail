ddcb7871dfb617e1f8a90242d025093875ac2c0b
Filip Wojda <wojdafilipdev@gmail.com>
2023-11-18 01:06:34 -0500

    route handler fix

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -1,14 +1,14 @@
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   const cookieStore = cookies();
   const json = await req.json();
-  console.log(json);
   const supabase = createClient(cookieStore);
   const { error, status, data } = await supabase.from('campaigns').insert(json);
   console.log(status);
   console.log(error);
 
-  return Response.json({ status });
+  return NextResponse.json({ status });
 }
